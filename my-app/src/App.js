9990e0691f50e19b4e330c75f3333e98e0a25fde
briws <briws@pm.me>
2021-03-12 12:43:00 +0300

    lesson 7 complete

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,17 +2,34 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
+    <div>
+      <Header />
+      <Technologies />
+    </div>);
+}
+
+const Technologies = () => {
+  return (
+    <div>
       <div>one div</div>
-      <ul>
-        <li>hoho</li>
-        <li>html</li>
-        <li>js</li>
-        <li>react</li>
-        <li>css</li>
-      </ul>
-    </div>
+        <ul>
+          <li>hohoho</li>
+          <li>html</li>
+          <li>js</li>
+          <li>react</li>
+          <li>css</li>
+        </ul>
+      </div>
   );
+}
+
+const Header = () => {
+  return (
+    <div>
+      <a href="#s">News</a>
+      <a href="#s">News Feed</a>
+      <a href="#s">Messeges</a>
+    </div>);
 }
 
 export default App;
