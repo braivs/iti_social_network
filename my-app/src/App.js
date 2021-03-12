c4e3ae6abaf030a19cbfe5203951a114683e8d48
briws <briws@pm.me>
2021-03-12 13:40:42 +0300

    lesson 9 complete

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,12 @@
 import './App.css';
+import Header from './Header.js';
+import Technologies from './Technologies.js';
 
 const App = () => {
   return (
     <div>
       <Header />
       <Technologies />
-    </div>);
-}
-
-const Technologies = () => {
-  return (
-    <div>
-      <div>one div</div>
-        <ul>
-          <li>hohoho</li>
-          <li>html</li>
-          <li>js</li>
-          <li>react</li>
-          <li>css</li>
-        </ul>
-      </div>
-  );
-}
-
-const Header = () => {
-  return (
-    <div>
-      <a href="#s">News</a>
-      <a href="#s">News Feed</a>
-      <a href="#s">Messeges</a>
     </div>);
 }
 
---
+++ my-app/src/Technologies.js
@@ -0,0 +1,16 @@
+const Technologies = () => {
+  return (
+    <div>
+      <div>one div</div>
+        <ul>
+        <li>hohoho</li>
+          <li>html</li>
+          <li>js</li>
+          <li>react</li>
+          <li>css</li>
+        </ul>
+      </div>
+  );
+}
+
+export default Technologies;
