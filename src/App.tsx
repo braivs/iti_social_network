8116d914584dfe55bc0826ff90f48de521ff285d
briws <briws@pm.me>
2021-03-19 18:18:47 +0300

    migrate to TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import './App.css';
-import Header from './Header.js';
-import Technologies from './Technologies.js';
+import Header from "./Header";
+import Technologies from "./Technologies";
 
 const App = () => {
   return (
@@ -9,5 +10,6 @@ const App = () => {
       <Technologies />
     </div>);
 }
+
 
 export default App;
