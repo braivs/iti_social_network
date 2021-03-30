bd9a3f4607a7166ff9d99d6cfc899e72d8181b72
briws <briws@pm.me>
2021-03-30 19:59:43 +0300

    12 complete

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+  return <header className={'header'}>
+    <img src="img/logo.png" alt=""/>
+  </header>
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className={'content'}>
+      <img src="img/main.jpg" alt=""/>
+      <div>
+        ava + description
+      </div>
+      <div>
+        My posts
+        <div>
+          New post
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile;
