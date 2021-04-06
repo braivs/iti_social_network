41f846a6a262f4867cdf8c064e2ab0b0e8d188cf
briws <briws@pm.me>
2021-04-06 21:39:38 +0300

    lesson 21 complete

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-// import s from './Dialogs.module.css'
+import s from './Dialogs.module.css'
 
 const Dialogs = () => {
   return (
-    <div>
-      Dialogs
+    <div className={s.dialogs}>
+      <div className={s.dialogItems}>
+        <div className={s.dialog + ' ' + s.active}>
+          Briws
+        </div>
+        <div className={s.dialog}>
+          Masha
+        </div>
+        <div className={s.dialog}>
+          Lera
+        </div>
+        <div className={s.dialog}>
+          Liza
+        </div><div className={s.dialog}>
+          Diana
+        </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.dialog}>Hi</div>
+        <div className={s.dialog}>Namaskar</div>
+        <div className={s.dialog}>Namaste</div>
+      </div>
     </div>
   )
 }
