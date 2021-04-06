0ce03bb9a7e5071c38d78dc21d3db65827749455
briws <briws@pm.me>
2021-04-06 17:02:39 +0300

    lesson 18 complete

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import s from './Profile.module.css'
-import MyPosts from "./MyPosts/MyPosts";
+import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
   return (
@@ -9,7 +9,7 @@ const Profile = () => {
       <div>
         ava + description
       </div>
-      <MyPosts />
+      <MyPosts/>
     </div>
   )
 }
