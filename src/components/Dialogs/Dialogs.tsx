e8f1a8fbdc94b0c2a54c95f5a5d06bb10eac32b1
briws <briws@pm.me>
2021-04-14 19:45:16 +0300

    24 complete + part of 25

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -25,22 +25,40 @@ const Message: React.FC<MessageType> = (props) => {
 }
 
 const Dialogs = () => {
+
+  let dialogsData = [
+    {id: 1,name: 'BriWS'},
+    {id: 2,name: 'Masha'},
+    {id: 3,name: 'Lera'},
+    {id: 4,name: 'Liza'},
+    {id: 5,name: 'Diana'},
+    {id: 6,name: 'Kristina'}
+  ]
+  let dialogsElements = [
+    <DialogItem id={dialogsData[0].id} name={dialogsData[0].name} />,
+    <DialogItem id={dialogsData[1].id} name={dialogsData[1].name} />,
+    <DialogItem id={dialogsData[2].id} name={dialogsData[2].name} />
+  ]
+
+  let messagesData = [
+    {id: 1, message: 'Hello'},
+    {id: 2, message: 'Namaskar'},
+    {id: 3, message: 'Namaste'},
+    {id: 4, message: 'Love'},
+    {id: 5, message: 'Peace'},
+    {id: 6, message: 'Yo'},
+  ]
+
   return (
     <div className={s.dialogs}>
       <div className={s.dialogItems}>
-        <DialogItem id={1} name={'Briws'} />
-        <DialogItem id={2} name={'Masha'} />
-        <DialogItem id={3} name={'Lera'} />
-        <DialogItem id={4} name={'Liza'} />
-        <DialogItem id={5} name={'Diana'} />
+        { dialogsElements }
+        {/*<DialogItem id={dialogsData[0].id} name={dialogsData[0].name} />
+        <DialogItem id={dialogsData[1].id} name={dialogsData[1].name} />*/}
       </div>
       <div className={s.messages}>
-        <Message message={'Hi'} />
-        <Message message={'Namaskar'} />
-        <Message message={'Namaste'} />
-        <Message message={'Love'} />
-        <Message message={'Peace'} />
-        <Message message={'Yo'} />
+        {/*<Message message={messagesData[0].message} />
+        <Message message={messagesData[1].message} />*/}
       </div>
     </div>
   )
