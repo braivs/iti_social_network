248404712fdf72620f0d995de192c96e6c373285
BriWS <brightwiths@gmail.com>
2021-06-22 19:00:24 +0300

    FAST_REFRESH=false

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -5,8 +5,6 @@ import {ProfileInfoPropsType} from './ProfileInfoContainer';
 export const ProfileInfo: React.FC<ProfileInfoPropsType> = (props) => {
   let element = <>
     <img src={props.top.imgSrc} alt=""/>
-    <div>111</div>
-
     <div className={s.descriptionBlock}>
       ava + description
     </div>
