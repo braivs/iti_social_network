cfac1d14fe9e63036073546a6c6cd797b824d4ee
BriWS <brightwiths@gmail.com>
2021-07-05 13:56:36 +0300

    warnings fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import {Route} from 'react-router-dom';
 import {Header} from './components/Header/Header';
 import {SidebarContainer} from './components/Sidebar/SidebarContainer';
 import {DialogsContainer} from './components/Dialogs/DialogsContainer';
-import {Profile} from './components/Profile/Profile';
 import {News} from './components/News/News';
 import {Music} from './components/Music/Music';
 import {Settings} from './components/Settings/Settings';
