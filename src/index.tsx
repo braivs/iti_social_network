6653fe284b3639131234498a9949d900e8ae8207
BriWS <brightwiths@gmail.com>
2021-10-19 12:30:12 +0300

    BrowserRouter -> HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {store} from './redux/redux-store';
 import App from "./App";
@@ -11,9 +11,9 @@ setInterval(() => {
 }, 1000)
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
         <App/>
     </Provider>
-  </BrowserRouter>, document.getElementById('root')
+  </HashRouter>, document.getElementById('root')
 );
